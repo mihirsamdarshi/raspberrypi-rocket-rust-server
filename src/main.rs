3183cd0130134011e879f564b2e92501caae2071
Mihir Samdarshi <mihirsamdarshi@yahoo.com>
2021-07-18 22:44:03 -0700

    change return to String

--- src/main.rs
+++ src/main.rs
@@ -6,47 +6,54 @@ use std::sync::{Arc, Mutex};
 use std::thread;
 
 use rocket::http::Status;
+use rocket::response::status;
+use rocket::response::status::Custom;
 use rocket::serde::json::Json;
 use rocket::State;
 
-use crate::experiment::{ExperimentInfo, RunState};
 use crate::state::TState;
+use crate::task::{RunState, TaskInfo};
 
-mod experiment;
 mod state;
+mod task;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[post("/run_experiment", format = "json", data = "<message>")]
-fn run_experiment_handler(
-    message: Json<ExperimentInfo>,
+#[post("/run_task", format = "json", data = "<message>")]
+fn run_task_handler(
+    message: Json<TaskInfo>,
     run_state: &State<RunState>,
     server_state: &State<Mutex<TState>>,
-) -> Status {
+) -> Custom<Option<String>> {
     let running = run_state.load(Ordering::SeqCst);
 
     if running {
-        return Status::ServiceUnavailable;
+        status::Custom(
+            Status::ServiceUnavailable,
+            Some(String::from("Service is already running")),
+        )
     } else {
-        let driver_clone = server_state.lock().unwrap().driver.clone();
         let worker_status_clone = Arc::clone(run_state);
-        server_state.lock().unwrap().threads.push(thread::spawn({
-            move || driver_clone.run_experiment(&message, worker_status_clone)
-        }));
-        run_state.swap(true, Ordering::SeqCst);
+        let driver_clone = server_state.lock().unwrap().driver.clone();
+        let port_check = driver_clone.check_port();
+        return match port_check {
+            Ok(msg) => {
+                server_state.lock().unwrap().threads.push(thread::spawn({
+                    move || driver_clone.run_task(&message, worker_status_clone)
+                }));
+                run_state.swap(true, Ordering::SeqCst);
+                status::Custom(Status::Accepted, Some(msg))
+            }
+            Err(msg) => status::Custom(Status::InternalServerError, Some(msg)),
+        };
     }
-
-    Status::Ok
 }
 
-#[get("/stop_experiment")]
-fn stop_experiment_handler(
-    run_state: &State<RunState>,
-    server_state: &State<Mutex<TState>>,
-) -> Status {
+#[get("/stop_task")]
+fn stop_task_handler(run_state: &State<RunState>, server_state: &State<Mutex<TState>>) -> Status {
     let running = run_state.load(Ordering::SeqCst);
 
     if !running {
@@ -70,10 +77,7 @@ fn not_found() -> Status {
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount(
-            "/",
-            routes![index, run_experiment_handler, stop_experiment_handler],
-        )
+        .mount("/", routes![index, run_task_handler, stop_task_handler])
         .register("/", catchers![not_found])
         .manage(Mutex::new(TState::new()))
         .manage(Arc::new(AtomicBool::new(false)))
